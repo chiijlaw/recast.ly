bb1af9be88ddc096f775f2da31d2081ca8fa652c
chiijlaw <chiithird@gmail.com>
2018-08-14 19:59:52 -0700

    solutions implimented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
-    
     this.state = {
       currentVideo: exampleVideoData[0],
       videoData: exampleVideoData,
@@ -12,24 +11,33 @@ class App extends React.Component {
       }
     };
     
-    
-    this.passVideoData = this.passVideoData.bind(this);
+    // this.passVideoData = this.passVideoData.bind(this);
     this.onVideoClick = this.onVideoClick.bind(this);
+  
   }
   
-  passVideoData() {
-    this.setState({
-      searchOptions: {
-        key: window.YOUTUBE_API_KEY,
-        query: $('#search').val(),
-        max: 5
-      },
-    });
-    searchYouTube(this.state.searchOptions, (data) => this.setState({//window.searchyoutube?
-      videoData: data //morespecific? htis.state.vidoedate?
-    })).bind(this);//using bind correctly?
-    console.log(videoData);
+  // passVideoData() {
+  //   console.log(this.state.searchOptions);
+  
+  //   this.props.search(this.state.searchOptions, (data) => this.setState({
+  //     videoData: data
+  //   }));
+  // }
+  
+  getYouTubeVideos(query) {
+    var opt = {
+      key: window.YOUTUBE_API_KEY,
+      query: query
+    };
+    
+    this.props.search(opt, (videos) => 
+      this.setState({
+        videos: videos,
+        currentVideo: videos[0]
+      })
+    );
   }
+  
   
   onVideoClick(props) {
     this.setState({
@@ -37,12 +45,13 @@ class App extends React.Component {
     });
   }
   
+  
   render() {
     return (
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><em>SEARCH: </em> <searchYouTube search={this.state.searchOptions} callback={this.onVideoClick} /><input type="text" id="search"/><input type="submit" onClick={() => this.passVideoData()}/></div>
+            <Search handleSearchInputChange={this.getYouTubeVideos.bind(this)}/>
           </div>
         </nav>
         <div className="row">
@@ -50,7 +59,7 @@ class App extends React.Component {
             <div><h5><em>videoPlayer</em> <VideoPlayer video={this.state.currentVideo} /></h5></div>
           </div>
           <div className="col-md-5">
-            <div><h5><em>Video List</em><VideoList videos={this.state.videoData} changeVid={this.onVideoClick}/></h5></div>
+            <div><h5><em>Video List</em> <VideoList videos={this.state.videoData} changeVid={this.onVideoClick}/></h5></div>
           </div>
         </div>
       </div>
@@ -58,12 +67,5 @@ class App extends React.Component {
   }
 }
 
-// {
-//   key: window.YOUTUBE_API_KEY,
-//   query: 'react',
-//   max: 5
-// }
-
 window.App = App;
-
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App search={window.searchYouTube}/>, document.getElementById("app"));
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -3,7 +3,9 @@ var searchYouTube = (option, callback) => {
     url: 'https://www.googleapis.com/youtube/v3/search',
     method: 'GET',
     data: {key: option.key, q: option.query, maxResults: option.max, part: 'snippet', type: 'video', videoEmbeddable: 'true'}
-  }).done((data) => (callback(data.items)));
+  }).done((data) => (callback(data.items))).fail(({responseJSON}) => {
+    responseJSON.error.errors.forEach((err) => console.error(err));
+  });
 };
 
 window.searchYouTube = searchYouTube;
